28b8e212ca7a1f4cf8af7012f25bca225fdfc97c
Jcleary8 <36216103+Jcleary8@users.noreply.github.com>
2018-02-06 21:57:51 -0500

    Initial commit

---
+++ app/src/main/java/com/klassykidz/www/kl/MainActivity.java
@@ -0,0 +1,34 @@
+package com.klassykidz.www.kl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("http://www.klassykidz.com/");
+        mywebView.setWebViewClient(new WebViewClient());0
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack())
+            mywebView.goBack();
+        else
+        super.onBackPressed();
+    }
+}
+
